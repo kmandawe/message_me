958ad15f086eff5a262911099d4471e9bfbfdbdc
kmandawe <kdmandawe06@gmail.com>
2023-06-26 17:47:34 +0800

    Restrict views at controller layer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
   
   def new
   end
@@ -22,5 +23,14 @@ class SessionsController < ApplicationController
     flash[:success] = "You have successfully logged out"
     redirect_to login_path
   end 
+  
+  private
+
+  public def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
+  end
   
 end
